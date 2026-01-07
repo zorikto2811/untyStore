0538f8940e43251360354a6961789cdc90b3df17
zorikto <zorikto.baklanov1990@gmail.com>
2026-01-07 21:21:34 +0800

    Описать сущность Boots

---
+++ domain/untyStore/Boots.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace untyStore
+{
+    public class Boots
+    {
+        public int Id { get; }
+
+        public string Title { get; }
+
+        public Boots(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
