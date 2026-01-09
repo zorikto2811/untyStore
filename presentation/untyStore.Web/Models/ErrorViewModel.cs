a8f72649a028590c69dc6ee1b702d8d10116f884
zorikto <zorikto.baklanov1990@gmail.com>
2026-01-09 19:00:58 +0800

    создать веб-приложение

---
+++ presentation/untyStore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace untyStore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
