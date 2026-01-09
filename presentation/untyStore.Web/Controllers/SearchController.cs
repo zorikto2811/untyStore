680cb42f968e51cde098bf26f1c2f1548c87bb30
zorikto <zorikto.baklanov1990@gmail.com>
2026-01-09 20:06:14 +0800

    Добавить страницу поиска

---
+++ presentation/untyStore.Web/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace untyStore.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IBootsRepository bootsRepository;
+
+        public SearchController(IBootsRepository bootsRepository)
+        {
+            this.bootsRepository = bootsRepository;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var boots = bootsRepository.GetAllByTitle(query);
+            return View(boots);
+        }
+    }
+}
--- presentation/untyStore.Web/Startup.cs
+++ presentation/untyStore.Web/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using untyStore.Memory;
 
 namespace untyStore.Web
 {
@@ -24,6 +25,7 @@ namespace untyStore.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<IBootsRepository, BootsRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
